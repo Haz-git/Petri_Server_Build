fc19d8ef518dd64900f300b11eb53c2890c8a426
Haz-git <zhouharry1@gmail.com>
2020-12-07 10:53:55 -0800

    Added profile pic appending to DB

--- backend/controllers/settingsController.js
+++ backend/controllers/settingsController.js
@@ -3,14 +3,29 @@ const handleAsync = require("../utils/handleAsync");
 
 //Model:
 const User = require('../models/userModels');
+const { response } = require("express");
 
 
 exports.addProfilePicToUser = handleAsync(async (req, res) => {
     const { _id, imgURL, imgConstraints } = req.body;
 
-    console.log(_id, imgURL, imgConstraints);
+    let userExistingImg = await User.findOne({ _id }).select('profileImg');
+
+    userExistingImg.profileImg['url'] = imgURL;
+    userExistingImg.profileImg['constraints'] = imgConstraints;
+
+
+    await User.updateOne({ _id }, { profileImg: userExistingImg.profileImg }, { bypassDocumentValidation: true}, (err) => {
+        if (err) console.log(err);
+    });
+
+    const responseUpdatedProfilePic = await User.findOne({ _id }).select('profileImg');
+
+    console.log(responseUpdatedProfilePic);
 
     res.status(200).json({
         status: 'Success',
+        responseUpdatedProfilePic,
     })
+
 })
