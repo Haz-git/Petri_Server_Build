5208e61539cd7762ade7c2005c517ffee9f6ffac
Haz-git <zhouharry1@gmail.com>
2020-12-07 09:24:45 -0800

    Created Controller function to process User settings updates

---
+++ backend/controllers/settingsController.js
@@ -0,0 +1,12 @@
+//Helper Function:
+const handleAsync = require("../utils/handleAsync");
+
+//Model:
+const User = require('../models/userModels');
+
+
+exports.addProfilePicToUser = handleAsync(async (req, res) => ) {
+    const { _id, imgURL, imgConstraints } = req.body;
+
+    console.log(_id, imgURL, imgConstraints);
+}
