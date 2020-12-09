a729ef62152858522c930d89b959470c942eb153
Haz-git <zhouharry1@gmail.com>
2020-12-09 08:08:10 -0800

    Adding controller functions for user settings

--- backend/controllers/settingsController.js
+++ backend/controllers/settingsController.js
@@ -7,11 +7,11 @@ const User = require('../models/userModels');
 exports.getProfilePicture = handleAsync(async (req, res) => {
     const { _id } = req.body;
 
-    let userExistingImg = await User.findOne({ _id }).select('profileImg');
+    const existingUser = await User.findOne({ _id });
 
     res.status(200).json({
         status: 'Success',
-        userExistingImg,
+        existingUser
     })
 
 })
@@ -30,13 +30,72 @@ exports.addProfilePicToUser = handleAsync(async (req, res) => {
         if (err) console.log(err);
     });
 
-    const responseUpdatedProfilePic = await User.findOne({ _id }).select('profileImg');
+    const responseUpdatedProfilePic = await User.findOne({ _id });
 
     console.log(responseUpdatedProfilePic);
 
     res.status(200).json({
         status: 'Success',
         responseUpdatedProfilePic,
+    })
+
+})
+
+/*
+router
+    .route('/settings/changeLastName')
+    .post(settingsController.userChangeLastName);
+
+router
+    .route('/settings/changeFirstName')
+    .post(settingsController.userChangeFirstName);
+
+router
+    .route('/settings/changeUserName')
+    .post(settingsController.userChangeUserName);
+
+router
+    .route('/settings/changeEmailAddress')
+    .post(settingsController.userChangeLastName);
+
+
+*/
+
+exports.userChangeLastName = handleAsync(async(req, res) => {
+    const { _id, newLastName } = req.body;
+
+    let existingUser = await User.findOne({ _id });
+    existingUser['lastName'] = newLastName;
+
+    await User.updateOne({ _id }, { lastName: existingUser.lastName }, { bypassDocumentValidation: true}, (err) => {
+        if (err) console.log(err);
+    });
+
+    const updatedLastNameUser = await User.findOne({ _id });
+
+    res.status(200).json({
+        status: 'Success',
+        updatedLastNameUser
+    })
+
+})
+
+
+exports.userChangeFirstName = handleAsync(async(req, res) => {
+    const { _id, newFirstName } = req.body;
+
+    let existingUser = await User.findOne({ _id });
+    existingUser['firstName'] = newFirstName;
+
+    await User.updateOne({ _id }, { firstName: existingUser.firstName }, { bypassDocumentValidation: true}, (err) => {
+        if (err) console.log(err);
+    });
+
+    const updatedFirstNameUser = await User.findOne({ _id });
+
+    res.status(200).json({
+        status: 'Success',
+        updatedFirstNameUser
     })
 
 })
