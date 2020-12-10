6546d8d5ddf897b41d48dc050d821dcc46f2b585
Haz-git <zhouharry1@gmail.com>
2020-12-10 12:40:36 -0800

    Attempting to debug CORs policy

--- backend/app.js
+++ backend/app.js
@@ -64,14 +64,18 @@ app.use(function(req, res, next) {
     res.header('Content-Type', 'application/json;charset=UTF-8');
     res.header('Access-Control-Allow-Credentials', true)
     res.header(
+        "Access-Control-Allow-Methods",
+        "GET,HEAD,PUT,POST,OPTIONS,UPDATE,DELETE"
+    )
+    res.header(
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept',
-      'Access-Control-Allow-Origin'
+      'Access-Control-Allow-Origin',
     )
     res.header({
         credentials: 'include',
     })
-    next()
+    next();
 })
 
 
