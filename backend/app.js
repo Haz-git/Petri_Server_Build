dd6cf54defba475ffbd075016359cd273d9e250a
Haz-git <zhouharry1@gmail.com>
2020-12-10 12:50:41 -0800

    reverting

--- backend/app.js
+++ backend/app.js
@@ -64,10 +64,6 @@ app.use(function(req, res, next) {
     res.header('Content-Type', 'application/json;charset=UTF-8');
     res.header('Access-Control-Allow-Credentials', true)
     res.header(
-        "Access-Control-Allow-Methods",
-        "GET,HEAD,PUT,POST,OPTIONS,UPDATE,DELETE"
-    )
-    res.header(
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept',
       'Access-Control-Allow-Origin',
