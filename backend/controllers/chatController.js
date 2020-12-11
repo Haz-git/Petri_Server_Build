a8b485ee8bbfdfb90a3e90f89da1648d21d30a95
Haz-git <zhouharry1@gmail.com>
2020-12-11 08:56:38 -0800

    Only rendering last 15 chat logs for memory conservation

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -8,6 +8,10 @@ exports.chatLogs = handleAsync(async( req, res, next) =>{
         .exec((err, chatLogs) => {
             if (err) return res.status(400).send(err);
             
+            //In order to conserve memory, currently only the last 15 chatlogs will be rendered:
+
+            chatLogs = chatLogs.slice(Math.max(chatLogs.length - 15, 1));
+            
             res.status(200).json({
                 status: 'Success',
                 // token,
