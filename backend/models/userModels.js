e25b0ec7d4710fb879b33877bcf150148ae3a176
Haz-git <zhouharry1@gmail.com>
2020-12-31 08:30:53 -0800

    Testing

--- backend/models/userModels.js
+++ backend/models/userModels.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
         required: [true, 'A user requires a first name!']
