ed042d548a4a716e9cfd3cbc921a1394f4ac0575
Haz-git <zhouharry1@gmail.com>
2021-07-13 13:43:34 -0700

    Removed split--bearer no longer there

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -17,10 +17,10 @@ exports.authenticateJWT = (req, res, next) => {
 
     const authHeader = req.headers.authorization;
 
-    if (authHeader) {
-        const token = authHeader.split(' ')[1];
 
-        jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (authHeader) {
+
+        jwt.verify(authHeader, process.env.JWT_SECRET, (err, user) => {
             if (err) {
                 return res.sendStatus(403);
             }
