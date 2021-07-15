1445d0a0d05b3f8ea66c47d622cebc7f4a289b85
Haz-git <zhouharry1@gmail.com>
2021-07-15 14:13:34 -0700

    Created dummy responses

--- backend/controllers/folderControllers.js
+++ backend/controllers/folderControllers.js
@@ -5,17 +5,29 @@ const handleAsync = require("../utils/handleAsync");
 const User = require('../models/userModels');
 
 exports.getFolders = handleAsync(async(req, res) => {
-
+    res.status(200).json({
+        status: 'Success',
+        msg: 'Route established1'
+    });
 });
 
 exports.addFolder = handleAsync(async(req, res) => {
-
+    res.status(200).json({
+        status: 'Success',
+        msg: 'Route established2'
+    });
 });
 
 exports.deleteFolder = handleAsync(async(req, res) => {
-
+    res.status(200).json({
+        status: 'Success',
+        msg: 'Route established3'
+    });
 });
 
 exports.renameFolder = handleAsync(async(req, res) => {
-
+    res.status(200).json({
+        status: 'Success',
+        msg: 'Route established4'
+    });
 });
