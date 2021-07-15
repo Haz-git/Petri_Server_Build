2fc3fe2e2491f4aebd27433e9b7a8f41160f806b
Haz-git <zhouharry1@gmail.com>
2021-07-15 14:07:18 -0700

    Setup controller functions for folder input

---
+++ backend/controllers/folderControllers.js
@@ -0,0 +1,21 @@
+//Helper Function:
+const handleAsync = require("../utils/handleAsync");
+
+//Model:
+const User = require('../models/userModels');
+
+exports.getFolders = handleAsync(async(req, res) => {
+
+});
+
+exports.addFolder = handleAsync(async(req, res) => {
+
+});
+
+exports.deleteFolder = handleAsync(async(req, res) => {
+
+});
+
+exports.renameFolder = handleAsync(async(req, res) => {
+
+});
